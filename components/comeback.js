f60a7a145fee7b5d3543df6c6896c06200864291
Gunnar Unger <gunnar.unger@gmail.com>
2019-10-31 09:27:01 +1100

    update

--- components/comeback.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    var modules={
-        "panel-main-comeback":    			 {url:"$A/modules/elig-questions-data.html",Table:"cleverlight-recruitment-record"},
-   }
-    for(m in modules){$vm.module_list[m]=modules[m];}
-})();

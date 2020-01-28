2516e04aa0750eeb6be796eefe5c9a8074ee2334
Gunnar Unger <gunnar.unger@gmail.com>
2020-01-28 17:23:03 +1100

    update

--- module-list.js
+++ module-list.js
@@ -1,11 +1,13 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "p1":                         {url:"$H/m/page-01.html",router:1},
-        "p2":                         {url:"$H/m/page-02.html",router:1},
-        "p3":                         {url:"$H/m/page-03.html",router:1},
-        "p4":                         {url:"$H/m/page-04.html",router:1},
-        "p5":                         {url:"$H/m/page-05.html",router:1},
+        "cart":     {url:"$H/m/cart.html",table:"comeback-cart"},
+        "footer":   {url:"$H/0/footer.html", Table:"comeback-email-address"},
+        "p1":       {url:"$H/m/page-01.html",router:1},
+        "p2":       {url:"$H/m/page-02.html",router:1},
+        "p3":       {url:"$H/m/page-03.html",router:1},
+        "p4":       {url:"$H/m/page-04.html",router:1},
+        "p5":       {url:"$H/m/page-05.html",router:1},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 

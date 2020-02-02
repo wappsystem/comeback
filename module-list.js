1ecfe1f86843123aff05c5a2aaf90697f795e1d4
Gunnar Unger <gunnar.unger@gmail.com>
2020-02-03 08:41:36 +1100

    update

--- module-list.js
+++ module-list.js
@@ -4,6 +4,7 @@
         "cart":     {url:"$H/m/cart.html",table:"comeback-cart"},
         "footer":   {url:"$H/0/footer.html", Table:"comeback-email-address"},
         "checkout1":{url:"$H/m/checkout1.html"},
+        "checkout2":{url:"$H/m/checkout2.html"},
         "p2":       {url:"$H/m/page-02.html",router:1},
         "p3":       {url:"$H/m/page-03.html",router:1},
         "p4":       {url:"$H/m/page-04.html",router:1},
